7778a1e757c5a3bc0cec167ab8d66afcca146228
Justin Duan <3116637679@qq.com>
2022-07-08 02:58:48 +0800

    fix bug: wrong order of r and s

--- PKI.sol
+++ PKI.sol
@@ -8,20 +8,19 @@ contract PKI{
         address registeringAddress = utils.generateAddressFromPublickey(publicKey);
         string memory message = utils.addressToString(msg.sender);
  
-        if (utils.verifySignature(message, v, s, r) != registeringAddress) {
+        if (utils.verifySignature(message, v, r, s) != registeringAddress) {
             return false;
         }
 
         id_pk_Map[msg.sender] = publicKey;
         return true;
     }
-
     function update(bytes memory publicKeyOld ,uint8 vOld, bytes32 rOld, bytes32 sOld,
     bytes memory publicKeyNew ,uint8 vNew, bytes32 rNew, bytes32 sNew) public returns(bool) {
         address registeringAddressOld = utils.generateAddressFromPublickey(publicKeyOld);
         string memory message = utils.addressToString(msg.sender);
 
-        if (utils.verifySignature(message, vOld, sOld, rOld) != registeringAddressOld) {
+        if (utils.verifySignature(message, vOld, rOld, sOld) != registeringAddressOld) {
             return false;
         }
 
@@ -31,7 +30,7 @@ contract PKI{
 
         address registeringAddressNew = utils.generateAddressFromPublickey(publicKeyNew);
 
-        if (utils.verifySignature(message, vNew, sNew, rNew) != registeringAddressNew) {
+        if (utils.verifySignature(message, vNew, rNew, sNew) != registeringAddressNew) {
             return false;
         }
 
@@ -44,7 +43,7 @@ contract PKI{
         string memory revokePrefix = "revoke";
         string memory revokeAddress = utils.addressToString(msg.sender);
         string memory message = string(abi.encodePacked(revokePrefix, revokeAddress));
-        if (utils.verifySignature(message, v, s, r) != registeringAddress) {
+        if (utils.verifySignature(message, v, r, s) != registeringAddress) {
             return false;
         }
 
